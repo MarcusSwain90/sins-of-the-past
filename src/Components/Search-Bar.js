5fd6f6958c549747b81295251acebcd89d977187
Marcus Swain <marcusswain@Marcuss-MacBook-Pro.local>
2018-12-05 18:29:04 -0500

    added constructor, local state and super props to search bar

--- src/Components/Search-Bar.js
+++ src/Components/Search-Bar.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 
-export default class SearchBar extends Component {
+ class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: ""};
+  }
+  
   render() {
     return (
-      <div>
-        
+      <div className="search-bar">
+        <input
+        value={}
+        onChange={event => this.onInputChange(event.tartget.value)}
+        />
       </div>
     )
   }
 }
+
+export default SearchBar;
